dd0b19a28522a9086cd27cd8bf072ac79b876883
Echa Andrea Gustiar <echaandrea6603@gmail.com>
2025-06-15 15:19:50 +0700

    update ulasan riwayat

--- src/scripts/views/pages/riwayat.js
+++ src/scripts/views/pages/riwayat.js
@@ -1,63 +1,73 @@
 // const authToken = sessionStorage.getItem('authToken');
-const containerRiwayat = document.querySelector('.content-riwayat');
+const containerRiwayat = document.querySelector(".content-riwayat");
 
-fetch('https://mentalwell-backend.vercel.app/history', {
-  method: 'GET',
+fetch("https://mentalwell10-api-production.up.railway.app/history", {
+  method: "GET",
   headers: {
-    'Authorization': `Bearer ${authToken}`,
-    'Content-Type': 'application/json'
-  }
+    Authorization: `Bearer ${authToken}`,
+    "Content-Type": "application/json",
+  },
 })
-  .then(response => {
+  .then((response) => {
     if (!response.ok) {
-      throw new Error('Failed to fetch data. Status: ' + response.status);
+      throw new Error("Failed to fetch data. Status: " + response.status);
     }
     return response.json();
   })
-  .then(data => {
+  .then((data) => {
     if (data && Array.isArray(data)) {
       if (data.length === 0) {
-        const noDataElement = document.createElement('p');
-        noDataElement.textContent = 'Tidak ada riwayat konseling.';
+        const noDataElement = document.createElement("p");
+        noDataElement.textContent = "Tidak ada riwayat konseling.";
         containerRiwayat.appendChild(noDataElement);
       } else {
-        data.forEach(riwayat => {
-          const riwayatElement = document.createElement('div');
-          riwayatElement.classList.add('container-riwayat');
+        data.forEach((riwayat) => {
+          const riwayatElement = document.createElement("div");
+          riwayatElement.classList.add("container-riwayat");
 
           const isReviewFilled = riwayat.review !== null;
           const scheduleDateString = riwayat.schedule_date;
           const scheduleDate = new Date(scheduleDateString);
-          const optionsSchedule = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+          const optionsSchedule = {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          };
 
-          const formattedScheduleDate = scheduleDate.toLocaleDateString('id-ID', optionsSchedule)
+          const formattedScheduleDate = scheduleDate.toLocaleDateString(
+            "id-ID",
+            optionsSchedule
+          );
 
           let formattedType;
 
           if (riwayat.type == "chat") {
-            formattedType = "Chat"
+            formattedType = "Chat";
           }
 
           let formattedStatus;
 
           if (riwayat.status == "belum_selesai") {
-            formattedStatus = "Belum Selesai"
+            formattedStatus = "Belum Selesai";
           } else if (riwayat.status == "selesai") {
-            formattedStatus = "Selesai"
+            formattedStatus = "Selesai";
           }
 
           let formattedScheduleTime;
 
           if (riwayat.schedule_time == "13:00-14:00") {
-            formattedScheduleTime = "13.00 - 14.00"
+            formattedScheduleTime = "13.00 - 14.00";
           } else if (riwayat.schedule_time == "16:00-17:00") {
-            formattedScheduleTime = "16.00 - 17.00"
+            formattedScheduleTime = "16.00 - 17.00";
           } else if (riwayat.schedule_time == "19:30-20:30") {
-            formattedScheduleTime = "19.30 - 20.30"
+            formattedScheduleTime = "19.30 - 20.30";
           }
 
           riwayatElement.innerHTML = `
-            <img src="${riwayat.profile_image}" alt="Foto Psikolog" id="psychologPhoto" />
+            <img src="${
+              riwayat.profile_image
+            }" alt="Foto Psikolog" id="psychologPhoto" />
             <div class="info-riwayat">
               <div class="info-text">
                 <p>
@@ -69,10 +79,22 @@ fetch('https://mentalwell-backend.vercel.app/history', {
                 </div>
                 <div class="status-button">
                   <span class="status-riwayat">${formattedStatus}</span>
-                  <button type="button" data-counseling-id="${riwayat.id}" onclick="openUlasanPopup(${riwayat.id}, '${riwayat.status}')"
-                    ${riwayat.status === 'belum_selesai' || isReviewFilled ? 'disabled' : ''}
-                    style="${(riwayat.status === 'belum_selesai' || isReviewFilled) ? 'background-color: lightgray; color: gray; cursor: default' : ''}">
-                    ${isReviewFilled ? 'ULASAN TERISI' : 'ISI ULASAN'}
+                  <button type="button" data-counseling-id="${
+                    riwayat.id
+                  }" onclick="openUlasanPopup(${riwayat.id}, '${
+            riwayat.status
+          }')"
+                    ${
+                      riwayat.status === "belum_selesai" || isReviewFilled
+                        ? "disabled"
+                        : ""
+                    }
+                    style="${
+                      riwayat.status === "belum_selesai" || isReviewFilled
+                        ? "background-color: lightgray; color: gray; cursor: default"
+                        : ""
+                    }">
+                    ${isReviewFilled ? "ULASAN TERISI" : "ISI ULASAN"}
                   </button>
                 </div>
               </div>
@@ -82,7 +104,7 @@ fetch('https://mentalwell-backend.vercel.app/history', {
         });
       }
     } else {
-      console.error('Invalid data format received from the server.');
+      console.error("Invalid data format received from the server.");
     }
   })
-  .catch(error => console.error('Error fetching data from API:', error));
+  .catch((error) => console.error("Error fetching data from API:", error));
--- src/scripts/views/pages/ulasan.js
+++ src/scripts/views/pages/ulasan.js
@@ -1,36 +1,36 @@
-const authToken = sessionStorage.getItem('authToken');
+const authToken = sessionStorage.getItem("authToken");
 
 let currentCounselingId;
 
 function openUlasanPopup(counselingId, status) {
-  currentCounselingId = counselingId
-  document.getElementById('container-ulasan').style.display = 'flex';
-  document.getElementById('overlay').style.display = 'block';
-  var navbarLinks = document.querySelectorAll('.navbar');
+  currentCounselingId = counselingId;
+  document.getElementById("container-ulasan").style.display = "flex";
+  document.getElementById("overlay").style.display = "block";
+  var navbarLinks = document.querySelectorAll(".navbar");
   navbarLinks.forEach(function (link) {
-    link.style.pointerEvents = 'none';
+    link.style.pointerEvents = "none";
   });
 }
 
 function closeUlasanPopup() {
-  document.getElementById('container-ulasan').style.display = 'none';
-  document.body.classList.remove('popup-open');
-  document.getElementById('overlay').style.display = 'none';
-  var navbarLinks = document.querySelectorAll('.navbar');
+  document.getElementById("container-ulasan").style.display = "none";
+  document.body.classList.remove("popup-open");
+  document.getElementById("overlay").style.display = "none";
+  var navbarLinks = document.querySelectorAll(".navbar");
   navbarLinks.forEach(function (link) {
-    link.style.pointerEvents = 'auto';
+    link.style.pointerEvents = "auto";
   });
 }
 
 function authenticate(event) {
   event.preventDefault();
-  alert('Authentication logic goes here!');
+  alert("Authentication logic goes here!");
 }
 
 function submitUlasan(ulasan) {
   Swal.fire({
-    title: 'Memuat...',
-    text: 'Harap tunggu sejenak. Ulasan akan segera dikirim',
+    title: "Memuat...",
+    text: "Harap tunggu sejenak. Ulasan akan segera dikirim",
     allowOutsideClick: false,
     showCancelButton: false,
     showConfirmButton: false,
@@ -39,55 +39,61 @@ function submitUlasan(ulasan) {
     },
   });
 
-
-  fetch(`https://mentalwell-backend.vercel.app/history/counselings/${currentCounselingId}`, {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${authToken}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ review: ulasan })
-  })
-    .then(response => {
+  fetch(
+    `https://mentalwell10-api-production.up.railway.app/counseling/${currentCounselingId}/review`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ review: ulasan }),
+    }
+  )
+    .then((response) => {
       if (!response.ok) {
-        throw new Error('Failed to submit review. Status: ' + response.status);
+        throw new Error("Failed to submit review. Status: " + response.status);
       }
       return response.json();
     })
-    .then(data => {
+    .then((data) => {
       Swal.close();
 
       Swal.fire({
-        title: 'Berhasil Membuat Ulasan!',
-        icon: 'success',
+        title: "Berhasil Membuat Ulasan!",
+        icon: "success",
         showConfirmButton: false,
         timer: 2000,
       });
       updateButtonAfterSubmission(currentCounselingId);
     })
-    .catch(error => console.error('Error submitting review:', error))
+    .catch((error) => console.error("Error submitting review:", error))
     .finally(() => {
       closeUlasanPopup();
     });
 }
 
 function updateButtonAfterSubmission(counselingId) {
-  const buttonElement = document.querySelector(`button[data-counseling-id="${counselingId}"]`);
+  const buttonElement = document.querySelector(
+    `button[data-counseling-id="${counselingId}"]`
+  );
 
   if (buttonElement) {
     // Disable the button
     buttonElement.disabled = true;
 
     // Change the color and text
-    buttonElement.style.backgroundColor = 'lightgray';
-    buttonElement.style.color = 'gray';
-    buttonElement.textContent = 'ULASAN TERISI';
+    buttonElement.style.backgroundColor = "lightgray";
+    buttonElement.style.color = "gray";
+    buttonElement.textContent = "ULASAN TERISI";
   }
 }
 
-document.querySelector('.content-ulasan').addEventListener('submit', function (event) {
-  event.preventDefault();
-  const ulasan = document.getElementById('ulasan').value;
+document
+  .querySelector(".content-ulasan")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const ulasan = document.getElementById("ulasan").value;
 
-  submitUlasan(ulasan);
-});
+    submitUlasan(ulasan);
+  });
