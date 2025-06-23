1c10ce5d97ff6c383ce538189e1073ae415d02e5
Echa Andrea Gustiar <echaandrea6603@gmail.com>
2025-06-23 19:05:28 +0700

    update lupa sandi

--- src/scripts/views/pages/lupasandi.js
+++ src/scripts/views/pages/lupasandi.js
@@ -1,44 +1,48 @@
-const lupasandiForm = document.getElementById('lupasandi-form');
+const lupasandiForm = document.getElementById("lupasandi-form");
 
-lupasandiForm.addEventListener('submit', async (event) => {
+lupasandiForm.addEventListener("submit", async (event) => {
   event.preventDefault();
 
-  const email = document.getElementById('email').value;
+  const email = document.getElementById("email").value;
+  const token = localStorage.getItem("adminToken"); 
 
   try {
-    const response = await fetch('https://mentalwell10-api-production.up.railway.app/forgot-password', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ${token}',
-      },
-      body: JSON.stringify({ email }),
-    });
+    const response = await fetch(
+      "https://mentalwell10-api-production.up.railway.app/forgot-password",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ email }),
+      }
+    );
 
     const responseData = await response.json();
 
     if (response.ok) {
       Swal.fire({
-        title: 'Berhasil Mengirim Email!',
+        title: "Berhasil Mengirim Email!",
         text: responseData.message,
-        icon: 'success',
+        icon: "success",
         timer: 2000,
         showConfirmButton: false,
       });
     } else {
       Swal.fire({
-        title: 'Gagal Mengirim Email!',
-        text: responseData.message || 'Silahkan Coba Lagi',
-        icon: 'error',
+        title: "Gagal Mengirim Email!",
+        text: responseData.message || "Silahkan Coba Lagi",
+        icon: "error",
         showConfirmButton: true,
       });
     }
   } catch (error) {
-    console.error('Error during reset password:', error);
+    console.error("Error during reset password:", error);
     await Swal.fire({
-      title: 'Gagal Mengirim Email!',
-      text: 'Silahkan Coba Lagi',
-      icon: 'error',
+      title: "Gagal Mengirim Email!",
+      text: "Silahkan Coba Lagi",
+      icon: "error",
       showConfirmButton: true,
     });
   }
