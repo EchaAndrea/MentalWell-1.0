79a8de69056a8d7fb61ebe7b5e78d6a44eb77996
Echa Andrea Gustiar <echaandrea6603@gmail.com>
2025-07-11 16:05:56 +0700

    update artikel

--- src/scripts/views/pages/articel.js
+++ src/scripts/views/pages/articel.js
@@ -4,6 +4,11 @@ const apiUrl = "https://mentalwell10-api-production.up.railway.app/articles";
 
 loadingIndicator.style.display = "block";
 
+function truncateText(text, maxLength) {
+  if (text.length <= maxLength) return text;
+  return text.slice(0, maxLength) + "...";
+}
+
 fetch(apiUrl)
   .then((response) => response.json())
   .then((data) => {
@@ -13,6 +18,9 @@ fetch(apiUrl)
     (data.articles || []).forEach((articleData) => {
       const articleElement = document.createElement("article");
 
+      // Batasi isi artikel yang tampil, misal 120 karakter
+      const truncatedContent = truncateText(articleData.content, 120);
+
       articleElement.innerHTML = `
                     <div class="image-articel">
                     <img src="${articleData.image}" alt="articel">
@@ -20,7 +28,7 @@ fetch(apiUrl)
                     <div class="isi-articel">
                         <h2>${articleData.title}</h2>
                         <div class="content"> 
-                          <p id="contentParagraph">${articleData.content}</p>
+                          <p id="contentParagraph">${truncatedContent}</p>
                         </div>
                         <div class="button-articel">
                           <button type="button" onclick="redirectToDetail('${articleData.id}')"> Baca Selengkapnya</button>
